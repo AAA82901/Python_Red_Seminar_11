32e1ee8a80939e38eb19e1bdbdbf4d1c041e82a9
папин серый ноут <email@email>
2024-12-04 18:59:08 +0300

    ввод номера действия

--- personal_assistant/personal_assistant.py
+++ personal_assistant/personal_assistant.py
@@ -1,9 +1,23 @@
-welcome_msg: str = '''Добро пожаловать в Персональный помощник!
-Выберите действие:
-1. Управление заметками
-2. Управление задачами
-3. Управление контактами
-4. Управление финансовыми записями
-5. Калькулятор
-6. Выход'''
-print(welcome_msg)
+print('Добро пожаловать в Персональный помощник!')
+welcome_msg: str = '''Выберите действие:
+\t1. Управление заметками
+\t2. Управление задачами
+\t3. Управление контактами
+\t4. Управление финансовыми записями
+\t5. Калькулятор
+\t6. Выход
+Введите номер действия: '''
+
+
+def get_command_n_from_user() -> int:
+    inp = input(welcome_msg)
+    while not inp.isdigit() or (inp := int(inp)) < 1 or 6 < inp:
+        print('Ошибка ввода номера действия!')
+        inp = input(welcome_msg)
+    return inp
+
+
+command_n: int = get_command_n_from_user()
+while command_n != 6:
+    print('Выбранное действие:', command_n)
+    command_n: int = get_command_n_from_user()
